f1ff399b020b6049ec6a3d944e3b7b389ac8423b
Alec Chapman <abchapman93@gmail.com>
2020-06-19 14:58:19 -0600

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="target_matcher",
-    version="0.0.1",
+    version="0.0.2",
     description="A wrapper class for extended rule-based matching in spaCy.",
     author="medSpaCy",
     author_email="medspacy.dev@gmail.com",
